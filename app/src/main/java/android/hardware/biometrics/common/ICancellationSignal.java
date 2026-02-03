c6530390a225148585c2be08d6710b44d4432fd7
ryanistr <ridho.holy@gmail.com>
2026-02-03 07:14:06 +0700

    Add for stub FaceHal : DO NOT MODIFY

---
+++ app/src/main/java/android/hardware/biometrics/common/ICancellationSignal.java
@@ -0,0 +1,7 @@
+package android.hardware.biometrics.common;
+//* FACE STUBS AUTO-GENERATED FILE. DO NOT MODIFY. */
+import android.os.IInterface;
+import android.os.RemoteException;
+public interface ICancellationSignal extends IInterface {
+    void cancel() throws RemoteException;
+}
